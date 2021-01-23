23213c75b780ef80335e6adbbc9050fb1b02df74
Festus Ali <festusali@gmail.com>
2021-01-23 02:47:45 +0100

    Fixed Typo (from env.port —env.PORT) in express.js

--- express.js
+++ express.js
@@ -10,4 +10,4 @@ app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(serveStatic(__dirname + '/dist'));
 app.use(history());
 
-app.listen(process.env.port || 5000);
+app.listen(process.env.PORT || 5000);
